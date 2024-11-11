e2a1158276c2fc0516a5e1cd60178cfd7c05e300
shubham09082003 <shubham9082003@gmail.com>
2024-11-11 21:36:39 +0530

    Adding backend url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { BACKEND_URL } from './config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(BACKEND_URL)
+  }
 })
